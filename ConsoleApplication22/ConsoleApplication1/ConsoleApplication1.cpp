9010a988b20f3e92900de66c9ed8454a3de6df7f
Александр Гармаш <garmash_a@itstep.academy>
2023-05-03 18:48:12 +0300

    remove(int pos): видаляє ціле число в позиції pos з масиву. (1.5 баллів)

--- ConsoleApplication22/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication22/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,24 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+/* remove(int pos): видаляє ціле число в позиції pos з масиву. (1.5 баллів) */
+
+int* arr;
+int n;
+
+void remove(int pos) {
+    int* arr2 = new int[n - 1];
+    for (int i = 0; i < pos; i++) { arr2[i] = arr[i]; }
+    for (int i = pos+1; i < n; i++) { arr2[i-1] = arr[i]; }
+    delete[] arr;
+    arr = arr2;
+    n--;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cin >> n;
+    arr = new int[n];
+
+    remove(2);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
